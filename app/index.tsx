84919802010b955a458cc8dcf0280e8a8d3f3e58
Zepoide <santi.tamarit07@gmail.com>
2024-08-28 19:37:37 -0300

    push de todo

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import Contador from "@/components/Contador";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Contador></Contador>
+    </View>
+  );
+}
